97f1bc93662d2735a09d23cd1630e190c7d13a87
dilshanikatukurunda <dskwellala@gmail.com>
2025-10-23 23:59:01 +0530

    ongoing coding

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="card">
+        <img src="./assets/websiteColor.png" alt="profile picture"></img>
+        <h2>John Doe</h2>
+        <p>Blacksmith.</p>
+        </div>
+    )
+}
+
+export default Card;
