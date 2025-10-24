01cb8a2fffdaafda695c81fe53b9916f6c72520a
dilshanikatukurunda <dskwellala@gmail.com>
2025-10-24 23:44:19 +0530

    finished card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,11 @@
+import profilepic from "./assets/websiteColor.png";
+
 function Card(){
     return(
         <div className="card">
-        <img src="./assets/websiteColor.png" alt="profile picture"></img>
-        <h2>John Doe</h2>
-        <p>Blacksmith.</p>
+        <img className="card-img" src={profilepic} alt="profile picture"></img>
+        <h2 className="card-title">John Doe</h2>
+        <p className="card-text">Blacksmith.</p>
         </div>
     )
 }
